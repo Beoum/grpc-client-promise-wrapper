30441c6dba7e1d527e11e6867aa491a0f2b8665c
Beoum <qoqjagml123@naver.com>
2022-06-08 23:59:41 +0900

    chore(): code cleanup

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,6 @@
-// GrpcObject type을 import하도록 하자!
-
 declare type gRPCMethods = any;
 
 /**
- * @grpc/grpc-js을 이용해서 client를 생성할 때 load했던 .proto file에 정의된 rpc method가 반환된다는 걸 알려주자!
+ * The rpc method defined in the loaded .proto file is returned when creating the client using @grpc/grpc-js.
  */
-declare function promiseWrapper<T>(client: T): gRPCMethods; // generic T 를 GrpcObject 로 바꿀 것.
-
-export = promiseWrapper;
+export function promiseWrapper<T>(client: T): gRPCMethods;
